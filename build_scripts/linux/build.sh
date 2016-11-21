d3931ccb009219e3bf1c500ffc7dcda6889e8dbd
oscarkramer <okramer@radiantblue.com>
2016-11-21 11:07:59 -0500

    Added documentation

--- build_scripts/linux/build.sh
+++ build_scripts/linux/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Script to build all OMAR services jar files. This is a workaround for failing
+# top-level gradle build. Run from the containing directory with no arguments.
+
 pushd `dirname $0` >/dev/null
 export SCRIPT_DIR=`pwd -P`
 popd >/dev/null
@@ -9,13 +13,13 @@ popd >/dev/null
 
 . $SCRIPT_DIR/env.sh
 
-for app in ${O2_APPS[@]} ; do 
+for app in ${O2_APPS[@]} ; do
    echo "BUILDING: $app ..."
    pushd $OMAR_DEV_HOME/apps/$app
    ./gradlew assemble
    if [ $? -ne 0 ];then
        echo "BUILD ERROR: $x failed to build..."
        exit 1
-   fi 
-   popd  
+   fi
+   popd
 done
